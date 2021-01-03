a3212083b5bd7d71cf19ff5fa6f840631ef7fa0c
Juan Pablo <juanpa-1920@hotmail.com>
2021-01-03 17:56:38 -0500

    Creando componente y estilos del header

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <nav class="header">
+      <div class="header__title">
+        <label>Task Manager</label>
+      </div>
+      <div class="header__items">
+        <a href="#" class="header__items__item">Home</a>
+        <a href="#" class="header__items__item">Tasks</a>
+        <a href="#" class="header__items__item">Calendar</a>
+      </div>
+      <div class="header__user-info">
+        <label>Profile</label>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>

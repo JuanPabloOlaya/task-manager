8a6c7249c3e4f0bb394a0d745de3fbc7148da292
Juan Pablo <juanpa-1920@hotmail.com>
2021-01-03 19:36:51 -0500

    Creando rutas en el vue router

--- src/router/index.js
+++ src/router/index.js
@@ -4,14 +4,21 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 const routes = [
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  // },
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/Home.vue'),
+  },
+  {
+    path: '/tasks',
+    name: 'Tasks',
+    component: () => import('@/views/Tasks.vue'),
+  },
+  {
+    path: '/calendar',
+    name: 'Calendar',
+    component: () => import('@/views/Calendar.vue'),
+  },
 ];
 
 const router = new VueRouter({

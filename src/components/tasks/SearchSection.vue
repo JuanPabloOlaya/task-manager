8b61a7ec109b8d53ef968c0aceeaaa1a212ada20
Juan Pablo <juanpa-1920@hotmail.com>
2021-01-11 15:06:54 -0500

    Creando distribucion de la vista de tareas

---
+++ src/components/tasks/SearchSection.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="search-section">
+    <label>Search section</label>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'SearchSection',
+};
+</script>
--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -1,8 +1,21 @@
 <template>
-  <h1>Tasks</h1>
+  <div id="tasksView">
+    <search-section />
+    <div class="tools-section">
+      <label>Herramientas</label>
+    </div>
+    <div class="tasks-list-section">
+      <label>Lista de tareas</label>
+    </div>
+  </div>
 </template>
 <script>
+import SearchSection from '@/components/tasks/SearchSection.vue';
+
 export default {
   name: 'Tasks',
+  components: {
+    SearchSection,
+  },
 };
 </script>

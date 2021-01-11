739ee8daa580e578ac99f9621cc56165470699bf
Juan Pablo <juanpa-1920@hotmail.com>
2021-01-11 15:45:30 -0500

    Agregando funcionalidad de collapse a los filtros

--- src/components/tasks/SearchSection.vue
+++ src/components/tasks/SearchSection.vue
@@ -1,11 +1,39 @@
 <template>
-  <section class="search-section">
-    <label>Search section</label>
+  <section
+    class="search-section"
+    :class="{'collapsed': show}"
+  >
+    <div class="search-section__filters">
+      <label>Filters</label>
+    </div>
+    <div class="search-section__collapse-button" @click="show = !show">
+      <span>Show Filters</span>
+    </div>
   </section>
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex';
+
 export default {
   name: 'SearchSection',
+  methods: {
+    ...mapActions('tasks', [
+      'updateShowFilters',
+    ]),
+  },
+  computed: {
+    ...mapState('tasks', [
+      'showFilters',
+    ]),
+    show: {
+      get() {
+        return this.showFilters;
+      },
+      set(value) {
+        this.updateShowFilters(value);
+      },
+    },
+  },
 };
 </script>
--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -1,21 +1,28 @@
 <template>
   <div id="tasksView">
     <search-section />
-    <div class="tools-section">
+    <div class="tools-section" :class="{'collapsed': show}">
       <label>Herramientas</label>
     </div>
-    <div class="tasks-list-section">
+    <div class="tasks-list-section" :class="{'collapsed': show}">
       <label>Lista de tareas</label>
     </div>
   </div>
 </template>
 <script>
+import { mapState } from 'vuex';
+
 import SearchSection from '@/components/tasks/SearchSection.vue';
 
 export default {
   name: 'Tasks',
   components: {
     SearchSection,
+  },
+  computed: {
+    ...mapState('tasks', {
+      show: (state) => state.showFilters,
+    }),
   },
 };
 </script>

51f23b3f84df3355f6949778020b157b8d219ad3
Juan Pablo <juanpa-1920@hotmail.com>
2021-01-31 12:44:14 -0500

    Distribucion de los filtros

--- src/components/tasks/SearchSection.vue
+++ src/components/tasks/SearchSection.vue
@@ -1,17 +1,23 @@
 <template>
   <section
     class="search-section"
-    :class="{'collapsed': show}"
+    :class="{'search-section--collapsed': show}"
   >
     <div class="search-section__filters">
-      <label>Filters</label>
+        <div class="search-section__filter-item" v-for="(item, index) of n" v-bind:key="index">
+          <label>Filtro {{item}}</label>
+          <input type="text" class="form-input">
+        </div>
     </div>
     <div class="search-section__collapse-button" @click="show = !show">
       <span>
         Show Filters
         <i
           class="mdi"
-          :class="{'mdi-chevron-down': !show, 'mdi-chevron-up': show}"
+          :class="{
+            'mdi-chevron-down': !show,
+            'mdi-chevron-up': show
+          }"
         ></i>
       </span>
     </div>
@@ -23,6 +29,9 @@ import { mapState, mapActions } from 'vuex';
 
 export default {
   name: 'SearchSection',
+  data: () => ({
+    n: 4,
+  }),
   components: {},
   methods: {
     ...mapActions('tasks', [
